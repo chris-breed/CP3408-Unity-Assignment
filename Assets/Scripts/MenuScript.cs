a2241dd75a996e3232515b2e9c3d90385b1fe7d8
chris-breed <christopher.breed@my.jcu.edu.au>
2017-05-26 15:03:18 +1000

    Main menu added to game scene and disabled at start of game

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -6,19 +6,19 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuScript : MonoBehaviour {
+public class MenuScript : MonoBehaviour {
 
-    public GameObject mmc;
-    public GameObject sc;
+    public GameObject mainMenuCanvas;
+    public GameObject inGameHud;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         //Menu Variables
         Button onePlayerGameButton = GameObject.Find("Play1PGame").GetComponent<Button>();
         onePlayerGameButton.onClick.AddListener(start1PGame);
         Button twoPlayerGameButton = GameObject.Find("Play2PGame").GetComponent<Button>();
         twoPlayerGameButton.onClick.AddListener(startP2Game);
-	}
+    }
 
     private void start1PGame() {
         Debug.Log("get a friend");
@@ -26,6 +26,7 @@ public class MainMenuScript : MonoBehaviour {
 
     private void startP2Game() {
         //load testing scene
-        SceneManager.LoadScene("Testing");
+        mainMenuCanvas.SetActive(false);
+        inGameHud.SetActive(true);
     }
 }
