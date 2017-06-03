decf95d78b590920a6742cf5198ddd61a9e74f4b
Aidan H <cassowary4@gmail.com>
2017-06-03 13:28:33 +1000

    NPC
    working NPC prefab with simple AI
    explosions are now quicker

--- Assets/Scripts/Player Scripts/NPC.cs
+++ Assets/Scripts/Player Scripts/NPC.cs
@@ -22,6 +22,7 @@ public class NPC : Controller
     public int weaponType = 1; //1 = gunn
     public float weaponShootSpeed = 0.1f;
     public int weaponDamage = 1;
+    public int aimError = 10; //in degrees
 
     bool shooting = true;
 
@@ -39,33 +40,86 @@ public class NPC : Controller
     void Update()
     {
         float distanceToPlayer = Vector3.Distance(transform.position, target.position);
-        playerForward = 1;
-        turnInput = 1;
-        turnInput = -1;
-        turnInput = 0;
+        print("distance is " + distanceToPlayer);
         timer -= Time.deltaTime;
 
         if (shooting)
         {
+            turnMe();
             if (timer < 0)
             {
-                timer += weaponShootSpeed;
-                shootWeapon(player);
+                if (timer < 0)
+                {
+                    if (activeWeapon == 1)
+                        timer += gun_fire_rate;
+                    else
+                        timer += cannon_fire_rate;
+                    shootWeapon(player);
+                }
+            }
+            if (activeWeapon == 1)
+            {
+                if (distanceToPlayer >8f)
+                {
+                    shooting = false;
+                }
+                else if (distanceToPlayer < 1f)
+                {
+                    switchWeapon();
+                }
             }
         }
-        if (activeWeapon == 1)
+        else
         {
-            if (distanceToPlayer > 40)
+            turnInput = 0;
+            if (distanceToPlayer < 3f)
+            {
+                shooting = true;
+            }
+        }
+        if (activeWeapon == 0 && distanceToPlayer > 1f)
+        {
                 switchWeapon();
         }
-        else if (distanceToPlayer < 15)
+        if (distanceToPlayer > 2f)
         {
-            switchWeapon();
+            turnMe();
+            playerForward = 1;
         }
+        else if (distanceToPlayer < .4f)
+        {
+            playerForward = 0;
+        }
+
+    }
+
+    void turnMe()
+    {
+        Vector3 aim = target.position - transform.position; //vector pointing to target
+        //float turn = Vector3.Angle(aim, transform.forward);//returns POSITIVE angle between ugh
+        float turn_direction = Vector3.Cross(transform.forward, aim).y;
+        if (turn_direction < -aimError)
+        {    //workaround using cross product
+            //turn = -turn;
+            turnInput = -1;
+        }
+        else if (turn_direction > aimError)
+        {    //workaround using cross product
+            //turn = -turn;
+            turnInput = 1;
+        }
+        else
+        {
+            turnInput = 0;
+        }
+        //turnInput = turn/10;
+        if (turnInput < -0.5f) { turnInput = -0.5f; }
+        if (turnInput > 0.5f) { turnInput = 0.5f; }
     }
 
     void FixedUpdate()
     {
         updateForwardAndRotation(playerForward * speed, turnInput * turnSpeed);
+        //updateForwardAndRotation(playerForward * speed, 0);
     }
 }
